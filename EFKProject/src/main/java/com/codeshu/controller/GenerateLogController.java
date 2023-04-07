4166467998ded818fe2498979933b95c56d3748d
codeshu <13828965090@163.com>
2023-04-07 09:08:11 +0800

    feat：EFK日志

---
+++ EFKProject/src/main/java/com/codeshu/controller/GenerateLogController.java
@@ -0,0 +1,26 @@
+package com.codeshu.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 产生日志
+ *
+ * @author CodeShu
+ * @date 2023/4/3 9:01
+ */
+@RestController
+public class GenerateLogController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(GenerateLogController.class);
+
+	@GetMapping("log")
+	public String log() {
+		LOGGER.debug("GenerateLogController:debug");
+		LOGGER.info("GenerateLogController:info");
+		LOGGER.warn("GenerateLogController:warn");
+		LOGGER.error("GenerateLogController:error");
+		return "success";
+	}
+}
