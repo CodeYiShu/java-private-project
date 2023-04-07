7f1b49966ba1ab28661013fd4b60d877281490e2
codeshu <13828965090@163.com>
2023-04-07 15:23:05 +0800

    feat：日志打印工具类

--- EFKProject/src/main/java/com/codeshu/controller/GenerateLogController.java
+++ EFKProject/src/main/java/com/codeshu/controller/GenerateLogController.java
@@ -1,9 +1,13 @@
 package com.codeshu.controller;
 
+import com.codeshu.utils.LogFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 产生日志
@@ -22,6 +26,17 @@ public class GenerateLogController {
 		LOGGER.warn("GenerateLogController:warn");
 		LOGGER.error("GenerateLogController:error");
 		int i = 1 / 0;
+		return "success";
+	}
+
+	@GetMapping("logUtils")
+	public String logUtils() {
+		Map<String, Object> params = new HashMap<>();
+		params.put("key1", "value1");
+		params.put("key2", "value2");
+		params.put("key3", "value3");
+		LogFormatUtils.printError(Thread.currentThread().getStackTrace()[1], params);
+
 		return "success";
 	}
 }
---
+++ EFKProject/src/main/java/com/codeshu/utils/LogFormatUtils.java
@@ -0,0 +1,90 @@
+package com.codeshu.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * 日志输出格式化封装
+ */
+public class LogFormatUtils {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LogFormatUtils.class);
+
+	/**
+	 * Debug打印
+	 *
+	 * @param stackTrace 栈信息
+	 * @param params     参数
+	 */
+	public static void printDebug(StackTraceElement stackTrace, Map<String, Object> params) {
+		if (Objects.nonNull(stackTrace.getFileName())) {
+			LOGGER.debug("当前方法{}:{}() 打印结果:{}",
+					stackTrace.getFileName().replace(".java", ""),
+					stackTrace.getMethodName(),
+					appendParam(params));
+		}
+	}
+
+	/**
+	 * Info打印
+	 *
+	 * @param stackTrace 栈信息
+	 * @param params     参数
+	 */
+	public static void printInfo(StackTraceElement stackTrace, Map<String, Object> params) {
+		if (Objects.nonNull(stackTrace.getFileName())) {
+			LOGGER.info("当前方法{}:{}() 打印结果:{}",
+					stackTrace.getFileName().replace(".java", ""),
+					stackTrace.getMethodName(),
+					appendParam(params));
+		}
+	}
+
+	/**
+	 * Warn打印
+	 *
+	 * @param stackTrace 栈信息
+	 * @param params     参数
+	 */
+	public static void printWarn(StackTraceElement stackTrace, Map<String, Object> params) {
+		if (Objects.nonNull(stackTrace.getFileName())) {
+			LOGGER.warn("当前方法{}:{}() 打印结果:{}",
+					stackTrace.getFileName().replace(".java", ""),
+					stackTrace.getMethodName(),
+					appendParam(params));
+		}
+	}
+
+	/**
+	 * Error打印
+	 *
+	 * @param stackTrace 栈信息
+	 * @param params     参数
+	 */
+	public static void printError(StackTraceElement stackTrace, Map<String, Object> params) {
+		if (Objects.nonNull(stackTrace.getFileName())) {
+			LOGGER.error("当前方法{}:{}() 打印结果:{}",
+					stackTrace.getFileName().replace(".java", ""),
+					stackTrace.getMethodName(),
+					appendParam(params));
+		}
+	}
+
+	/**
+	 * 拼接参数
+	 */
+	private static String appendParam(Map<String, Object> params) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(" ");
+
+		for (Map.Entry<String, Object> entry : params.entrySet()) {
+			sb.append(entry.getKey()).append(": ").append(entry.getValue()).append(" ; ");
+		}
+
+		sb.deleteCharAt(sb.length() - 1);
+		String obj = sb.toString();
+		return obj.isEmpty() ? null : obj;
+	}
+}
