9757a8e657d4189f29f7431b2417143defe9f34d
codeshu <13828965090@163.com>
2023-05-10 15:16:42 +0800

    feat：Neo4j增加Neo4jClient执行Cypher和增加项目测试

--- neo4j-project/src/test/java/com/codeshu/YunShuoTest.java
+++ neo4j-project/src/test/java/com/codeshu/YunShuoTest.java
@@ -1,12 +1,13 @@
 package com.codeshu;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.lang.Snowflake;
 import cn.hutool.core.util.IdUtil;
-import cn.hutool.json.JSONUtil;
 import com.codeshu.entity.AttributeEntity;
 import com.codeshu.entity.BenTiEntity;
 import com.codeshu.repository.AttributeRepository;
 import com.codeshu.repository.BenTiRepository;
+import com.codeshu.request.BenTiRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,10 +37,10 @@ class YunShuoTest {
 	@Test
 	public void testBatchUpdateAndInsert() {
 		//当节点的ID为NULL时则表示新增节点，当节点的ID不为NULL时表示更新；当ID不为NULL但是库中没有此节点，也会自动创建
-		List<BenTiEntity> benTiEntityList = getBenTiEntityHasId();
+		List<BenTiRequest> requestList = getBenTiEntityHasId();
 
 		//查询出库中所有这些本体节点
-		List<Long> ids = benTiEntityList.stream().map(BenTiEntity::getId).filter(Objects::nonNull).collect(Collectors.toList());
+		List<Long> ids = requestList.stream().map(BenTiRequest::getId).filter(Objects::nonNull).collect(Collectors.toList());
 		List<BenTiEntity> DbBenTiEntityList = benTiRepository.findAllById(ids);
 		//获取这些本体节点的所有属性节点
 		List<List<AttributeEntity>> attributeEntityLists = DbBenTiEntityList.stream().map(BenTiEntity::getAttributeEntityList)
@@ -53,12 +54,13 @@ class YunShuoTest {
 		attributeRepository.deleteAllById(allAttributeIds);
 
 		//为本体节点和属性节点生成 id 字段值
-		generateIdToNode(benTiEntityList);
+		generateIdToNode(requestList);
 		//再去保存这些本体节点和她的属性节点
+		List<BenTiEntity> benTiEntityList = BeanUtil.copyToList(requestList, BenTiEntity.class);
 		benTiRepository.saveAll(benTiEntityList);
 
 		//本体节点之间的关系
-		List<String> cypherList = getCreateRelationShipCypher(benTiEntityList);
+		List<String> cypherList = getCreateRelationShipCypher(requestList);
 		for (String cypher : cypherList) {
 			neo4jClient.query(cypher).run();
 		}
@@ -67,25 +69,25 @@ class YunShuoTest {
 	/**
 	 * 生成本体节点和本体节点之间的关系 cypher 语句
 	 *
-	 * @param benTiEntityList 本体节点集合
+	 * @param requestList 本体节点集合
 	 * @return cypher语句
 	 */
-	public List<String> getCreateRelationShipCypher(List<BenTiEntity> benTiEntityList) {
+	public List<String> getCreateRelationShipCypher(List<BenTiRequest> requestList) {
 		//建立关系的固定语法
 		final String str = "MATCH (a:BenTi), (b:BenTi) WHERE a.id = %s AND b.id = %s MERGE (a)-[:%s]->(b);";
 		List<String> cypherList = new ArrayList<>();
 
-		for (BenTiEntity benTiEntity : benTiEntityList) {
+		for (BenTiRequest request : requestList) {
 			//获取当前本体节点的所有弧头本体节点
-			List<BenTiEntity> endBenTiEntityList = benTiEntity.getEndBenTiEntityList();
-			for (BenTiEntity endBenTiEntity : endBenTiEntityList) {
+			List<BenTiRequest> endBenTiEntityList = request.getEndBenTiEntityList();
+			for (BenTiRequest endBenTiEntity : endBenTiEntityList) {
 				//根据节点名称，将 id 字段值设置进弧头本体节点的 id 字段中
-				Long id = benTiEntityList.stream().filter(entity -> entity.getName().equals(endBenTiEntity.getName()))
-						.map(BenTiEntity::getId).filter(Objects::nonNull).findFirst().orElse(null);
+				Long id = requestList.stream().filter(entity -> entity.getName().equals(endBenTiEntity.getName()))
+						.map(BenTiRequest::getId).filter(Objects::nonNull).findFirst().orElse(null);
 				endBenTiEntity.setId(id);
 
 				//参数填充入cypher中
-				String format = String.format(str, benTiEntity.getId(), endBenTiEntity.getId(), endBenTiEntity.getRelationshipType());
+				String format = String.format(str, request.getId(), endBenTiEntity.getId(), endBenTiEntity.getRelationshipType());
 				cypherList.add(format);
 			}
 		}
@@ -95,17 +97,17 @@ class YunShuoTest {
 	/**
 	 * 生成自定义 id 字段值
 	 *
-	 * @param benTiEntityList 本体节点集合（包含其属性节点）
+	 * @param requestList 本体节点集合（包含其属性节点）
 	 */
-	public void generateIdToNode(List<BenTiEntity> benTiEntityList) {
+	public void generateIdToNode(List<BenTiRequest> requestList) {
 		Snowflake snowflake = IdUtil.getSnowflake();
 
-		for (BenTiEntity benTiEntity : benTiEntityList) {
+		for (BenTiRequest request : requestList) {
 			//为没有 id 的本体节点添加上 id
-			if (Objects.isNull(benTiEntity.getId())) {
-				benTiEntity.setId(snowflake.nextId());
+			if (Objects.isNull(request.getId())) {
+				request.setId(snowflake.nextId());
 			}
-			List<AttributeEntity> attributeEntityList = benTiEntity.getAttributeEntityList();
+			List<AttributeEntity> attributeEntityList = request.getAttributeEntityList();
 			//为所有属性节点添加上 id
 			for (AttributeEntity attributeEntity : attributeEntityList) {
 				attributeEntity.setId(snowflake.nextId());
@@ -113,45 +115,52 @@ class YunShuoTest {
 		}
 	}
 
-	public List<BenTiEntity> getBenTiEntityHasId() {
-		BenTiEntity entity1 = new BenTiEntity();
-		//entity1.setId(1656131971312533504L);
-		entity1.setName("施工工具");
+	/**
+	 * 模拟入参
+	 *
+	 * @return 参数
+	 */
+	public List<BenTiRequest> getBenTiEntityHasId() {
+		BenTiRequest request1 = new BenTiRequest();
+		//request1.setId(1656131971312533504L);
+		request1.setName("施工工具");
 		//当前本体属性指向当前本体
 		AttributeEntity attributeEntity1 = new AttributeEntity(null, "施工工具属性1", "String", true, false, 0, 100, 0, "备注");
 		AttributeEntity attributeEntity2 = new AttributeEntity(null, "施工工具属性2", "Integer", false, true, 0, 100, 0, "备注");
-		entity1.getAttributeEntityList().add(attributeEntity1);
-		entity1.getAttributeEntityList().add(attributeEntity2);
+		request1.getAttributeEntityList().add(attributeEntity1);
+		request1.getAttributeEntityList().add(attributeEntity2);
 
-		BenTiEntity entity2 = new BenTiEntity();
-		//entity2.setId(1656131971312533507L);
-		entity2.setName("接入式电子");
+		BenTiRequest request2 = new BenTiRequest();
+		//request2.setId(1656131971312533507L);
+		request2.setName("接入式电子");
 		//当前本体属性指向当前本体
 		AttributeEntity attributeEntity3 = new AttributeEntity(null, "接入式电子属性1", "String", true, false, 0, 100, 0, "备注");
 		AttributeEntity attributeEntity4 = new AttributeEntity(null, "接入式电子属性2", "Integer", false, true, 0, 100, 0, "备注");
-		entity2.getAttributeEntityList().add(attributeEntity3);
-		entity2.getAttributeEntityList().add(attributeEntity4);
+		request2.getAttributeEntityList().add(attributeEntity3);
+		request2.getAttributeEntityList().add(attributeEntity4);
 		//当前本体指向其他本体
-		BenTiEntity endBenTi1 = new BenTiEntity();
+		BenTiRequest endBenTi1 = new BenTiRequest();
 		endBenTi1.setRelationshipType("施工工具");
 		endBenTi1.setName("施工工具");
-		entity2.getEndBenTiEntityList().add(endBenTi1);
+		request2.getEndBenTiEntityList().add(endBenTi1);
 
-		BenTiEntity entity3 = new BenTiEntity();
-		//entity3.setId(1656131971312533510L);
-		entity3.setName("电子计量装置");
+		BenTiRequest request3 = new BenTiRequest();
+		//request3.setId(1656131971312533510L);
+		request3.setName("电子计量装置");
 		//当前本体指向其他本体
-		BenTiEntity endBenTi2 = new BenTiEntity();
+		BenTiRequest endBenTi2 = new BenTiRequest();
 		endBenTi2.setRelationshipType("分类");
 		endBenTi2.setName("接入式电子");
-		entity3.getEndBenTiEntityList().add(endBenTi2);
+		request3.getEndBenTiEntityList().add(endBenTi2);
 
-		return Arrays.asList(entity1, entity2, entity3);
+		return Arrays.asList(request1, request2, request3);
 	}
 
 	@Test
-	public void testQuery(){
+	public void testQuery() {
 		List<BenTiEntity> benTiEntityList = benTiRepository.findAll();
-		System.out.println(JSONUtil.parseArray(benTiEntityList));
+		for (BenTiEntity benTiEntity : benTiEntityList) {
+			System.out.println(benTiEntity);
+		}
 	}
 }
--- neo4j-project/src/test/java/com/codeshu/request/BenTiRequest.java
+++ neo4j-project/src/test/java/com/codeshu/request/BenTiRequest.java
@@ -1,12 +1,9 @@
-package com.codeshu.entity;
+package com.codeshu.request;
 
+import com.codeshu.entity.AttributeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.neo4j.core.schema.Id;
-import org.springframework.data.neo4j.core.schema.Node;
-import org.springframework.data.neo4j.core.schema.Property;
-import org.springframework.data.neo4j.core.schema.Relationship;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,25 +12,28 @@ import java.util.List;
  * @author CodeShu
  * @date 2023/5/9 15:14
  */
-@Node(labels = "BenTi")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class BenTiEntity {
+public class BenTiRequest {
 	/**
 	 * 自定义id
 	 */
-	@Id
 	private Long id;
 
 	/**
 	 * 本体名称
 	 */
-	@Property("name")
 	private String name;
 
+	/**
+	 * 关系类型
+	 */
+	private String relationshipType;
 
 	// 多个属性作为弧尾，指向当前本体
-	@Relationship(type = "ATTRIBUTE_IN", direction = Relationship.Direction.INCOMING)
 	private List<AttributeEntity> attributeEntityList = new ArrayList<>();
+
+	// 当前节点作为弧尾，指向其他本体节点
+	private List<BenTiRequest> endBenTiEntityList = new ArrayList<>();
 }
