6c70f1dfc6cc7c597ae750cffd110d72dc86948f
codeshu <13828965090@163.com>
2023-09-24 13:57:46 +0800

    feat： K8s模块搭建

---
+++ extend-project/k8s-module/src/main/java/com/codeshu/K8sModuleApplication.java
@@ -0,0 +1,13 @@
+package com.codeshu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sModuleApplication.class, args);
+	}
+
+}
