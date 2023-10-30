cdd8002746a0ec5cabdc8148efe66b3b1461a9a9
codeshu <13828965090@163.com>
2023-10-30 09:38:06 +0800

    feat：跨域

--- ai-big-model-project/src/main/java/com/codeshu/config/CorsConfig.java
+++ ai-big-model-project/src/main/java/com/codeshu/config/CorsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -14,6 +15,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+	// 或使用以下配置
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**")
+//				.allowedOriginPatterns("*")
+//				.allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+//				.allowCredentials(true)
+//				.maxAge(3600)
+//				.allowedHeaders("*");
+//	}
 
 	private CorsConfiguration buildConfig() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
