86c47cb59d2be362fc4209ce1c4aac7d3b8f7fed
codeshu <13828965090@163.com>
2024-03-09 20:42:31 +0800

    导入导出

--- file-import-export-project/src/main/java/com/codeshu/controller/ExportController.java
+++ file-import-export-project/src/main/java/com/codeshu/controller/ExportController.java
@@ -1,14 +1,21 @@
 package com.codeshu.controller;
 
+import cn.hutool.core.date.DateUtil;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -19,30 +26,55 @@ import java.util.UUID;
 public class ExportController {
 	@GetMapping("exportData")
 	public void exportData(HttpServletResponse response) {
+		// 列名称
+		List<String> columnNameList = Arrays.asList("ID", "姓名", "年龄");
+		// 每一行单元格的数量（列数）
+		int cellNum = columnNameList.size();
+
 		// 1、创建一个工作簿
 		XSSFWorkbook workbook = new XSSFWorkbook();
 
 		// 2、创建一个底部的工作表
 		XSSFSheet sheet = workbook.createSheet("sheet");
 
-		// 3、设置列宽度
-		sheet.setColumnWidth(0, 3000);
-		sheet.setColumnWidth(1, 9000);
-		sheet.setColumnWidth(2, 7000);
+		// 3、设置每个单元格宽度
+		for (int i = 0; i < cellNum; i++) {
+			sheet.setColumnWidth(i, 5000);
+		}
 
-		// 4、创建第一行
-		Row row1 = sheet.createRow(0);
-		// 5、为第一行添加单元格值，作为列名
-		row1.createCell(0).setCellValue("ID");
-		row1.createCell(1).setCellValue("姓名");
-		row1.createCell(2).setCellValue("年龄");
+		// 4、创建第一行，第一行是列名称
+		Row firstRow = sheet.createRow(0);
+		// 5、设置第一行的高度
+		firstRow.setHeight((short) 600);
+		// 6、设置第一行的列名称
+		for (int i = 0; i < cellNum; i++) {
+			Cell firstCell = firstRow.createCell(i);
+			firstCell.setCellValue(columnNameList.get(i));
+		}
 
-		// 6、循环插入6行
-		for (int i = 1; i < 6; i++) {
-			Row row = sheet.createRow(i);
-			row.createCell(0).setCellValue(i);
-			row.createCell(1).setCellValue("姓名" + i);
-			row.createCell(2).setCellValue("年龄" + i);
+		// 7、插入其他行数据（从第 2 行开始）
+		List<List<Object>> rowDataList = Arrays.asList(Arrays.asList(1, "张三", 12), Arrays.asList(2, "李四", 13));
+		int rowIndex = 1;
+		for (List<Object> rowData : rowDataList) {
+			// 创建当前行
+			Row row = sheet.createRow(rowIndex);
+			// 生成当前行的多个单元格
+			for (int i = 0; i < cellNum; i++) {
+				// 创建单元格
+				Cell cell = row.createCell(i);
+				// 单元格的值
+				Object cellValue = rowData.get(i);
+				if (Objects.isNull(cellValue)) {
+					cell.setCellValue("");
+				} else if (cellValue instanceof String) {
+					cell.setCellValue(StringUtils.hasText((String) cellValue) ? (String) cellValue : "");
+				} else if (cellValue instanceof Date) {
+					cell.setCellValue(DateUtil.format((Date) cellValue, "yyyy-MM-dd"));
+				} else {
+					cell.setCellValue(cellValue.toString());
+				}
+			}
+			rowIndex++;
 		}
 
 		try {
